2bbaddbe53827c32bf976e285940f80054f9f462
Lakshman Karunasena <lakkaru@gmail.com>
2023-06-27 12:40:43 +0530

    feat- new component created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'first_angular_project';
+  title = 'Lakshman Karunasena';
+  day='Today is Monday';
 }
